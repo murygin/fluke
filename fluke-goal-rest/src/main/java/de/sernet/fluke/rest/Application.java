f338b51d043c688561590d2c7b5988522c06a81d
Daniel Murygin <dm@sernet.de>
2016-04-03 19:47:42 +0200

    Add dual build configuration to build a JAR or a WAR See: http://jangalinski.github.io/2015/02/28/deploy-springboot-as-war/

--- fluke-goal-rest/src/main/java/de/sernet/fluke/rest/Application.java
+++ fluke-goal-rest/src/main/java/de/sernet/fluke/rest/Application.java
@@ -2,6 +2,8 @@ package de.sernet.fluke.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +12,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("de.sernet.fluke")
 @EntityScan("de.sernet.fluke.persistence")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+        
+        @Override
+        protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+            return builder.sources(this.getClass());
+        }
 }
--- fluke-goal-vaadin-gui/src/main/java/de/sernet/fluke/gui/vaadin/FlukeGoalVaadinGuiApplication.java
+++ fluke-goal-vaadin-gui/src/main/java/de/sernet/fluke/gui/vaadin/FlukeGoalVaadinGuiApplication.java
@@ -2,11 +2,18 @@ package de.sernet.fluke.gui.vaadin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlukeGoalVaadinGuiApplication {
+public class FlukeGoalVaadinGuiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlukeGoalVaadinGuiApplication.class, args);
 	}
+        
+        @Override
+        protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+            return builder.sources(this.getClass());
+        }
 }
