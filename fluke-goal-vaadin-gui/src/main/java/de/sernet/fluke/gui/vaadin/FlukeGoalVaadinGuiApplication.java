dd94e47caa3921d5b1edddb4086f28b196d2e791
Daniel Murygin <dm@sernet.de>
2016-04-03 14:13:38 +0200

    Add a VaadinUI class to module vaadin-gui.
    Move classes in module vaadin-gui to package de.sernet.fluke.gui.vaadin.

--- fluke-goal-vaadin-gui/src/main/java/de/sernet/fluke/gui/vaadin/FlukeGoalVaadinGuiApplication.java
+++ fluke-goal-vaadin-gui/src/main/java/de/sernet/fluke/gui/vaadin/FlukeGoalVaadinGuiApplication.java
@@ -1,4 +1,4 @@
-package de.sernet;
+package de.sernet.fluke.gui.vaadin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- fluke-goal-vaadin-gui/src/test/java/de/sernet/fluke/gui/vaadin/test/FlukeGoalVaadinGuiApplicationTests.java
+++ fluke-goal-vaadin-gui/src/test/java/de/sernet/fluke/gui/vaadin/test/FlukeGoalVaadinGuiApplicationTests.java
@@ -1,8 +1,11 @@
-package de.sernet;
+package de.sernet.fluke.gui.vaadin.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import de.sernet.fluke.gui.vaadin.FlukeGoalVaadinGuiApplication;
+
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
