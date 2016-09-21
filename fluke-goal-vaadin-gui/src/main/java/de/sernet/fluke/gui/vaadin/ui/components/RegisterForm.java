dd585570af7ca87d217d5a85b4a657d2ba62ef06
Daniel Murygin <daniel.murygin@gmail.com>
2016-09-21 07:51:37 +0000

    Prepare workspace for presentation

--- fluke-goal-vaadin-gui/src/main/java/de/sernet/fluke/gui/vaadin/ui/components/RegisterForm.java
+++ fluke-goal-vaadin-gui/src/main/java/de/sernet/fluke/gui/vaadin/ui/components/RegisterForm.java
@@ -43,9 +43,9 @@ public class RegisterForm extends FormLayout {
 
     private static final Logger LOG = LoggerFactory.getLogger(RegisterForm.class);
 
-    private final TextField login = new TextField("Login name");
+    private final TextField login = new TextField("login name");
 
-    private final TextField getFirstName = new TextField("First name");
+    private final TextField getFirstName = new TextField("first name");
 
     private final TextField getLastName = new TextField("last name");
 
